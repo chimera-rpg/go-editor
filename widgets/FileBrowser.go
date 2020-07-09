c269f887f4d08217cb5b8cccda6be0a223c12622
kts of kettek (yan) <kettek1@kettek.net>
2020-07-08 23:20:48 -0700

    Rework FileBrowser

--- widgets/FileBrowser.go
+++ widgets/FileBrowser.go
@@ -24,16 +24,16 @@ func FileBrowser(cwd *string, filename *string, favorites []string) *FileBrowser
 
 func (f *FileBrowserWidget) Build() {
 	// Render Favorites
-	imgui.BeginChildV("left", imgui.Vec2{X: 200, Y: 0}, true, 0)
+	imgui.BeginGroup()
 	{
 	}
-	imgui.EndChild()
+	imgui.EndGroup()
 	imgui.SameLine()
 	// Render Path
-	imgui.BeginChildV("right", imgui.Vec2{X: 0, Y: 0}, true, 0)
+	imgui.BeginGroup()
 	{
-		// Begin Address
-		imgui.BeginChildV("address", imgui.Vec2{X: 0, Y: 50}, true, 0)
+		// Address Begin
+		imgui.BeginGroup()
 		{
 			pathParts := strings.Split(*f.cwd, fmt.Sprintf("%c", filepath.Separator))
 			for i, p := range pathParts {
@@ -46,29 +46,38 @@ func (f *FileBrowserWidget) Build() {
 				imgui.SameLine()
 			}
 		}
-		imgui.EndChild()
-		imgui.BeginChildV("browser", imgui.Vec2{X: 0, Y: 0}, true, 0)
+		imgui.EndGroup()
+		// Address End
+		// Browser Begin
+		imgui.BeginGroup()
+		imgui.BeginChildV("browser", imgui.Vec2{X: 0, Y: 300}, false, 0)
 		{
 			files, err := ioutil.ReadDir(*f.cwd)
 			if err == nil {
 				for _, file := range files {
 					if imgui.ButtonV(file.Name(), imgui.Vec2{X: 0, Y: 0}) {
-						*f.cwd = fmt.Sprintf("%s%c%s", *f.cwd, filepath.Separator, file.Name())
+						if file.IsDir() {
+							*f.cwd = fmt.Sprintf("%s%c%s", *f.cwd, filepath.Separator, file.Name())
+						} else {
+							*f.filename = file.Name()
+						}
 						// TODO: move/open file
 					}
 				}
 			}
 		}
 		imgui.EndChild()
-		// Final name path
-		imgui.BeginChildV("name", imgui.Vec2{X: 0, Y: 0}, true, 0)
+		imgui.EndGroup()
+		// Browser End
+		// FileName Begin
+		imgui.BeginGroup()
 		{
 			if imgui.InputTextV("filename", f.filename, 0, nil) {
 				// TODO: ... something?
 			}
 		}
-		imgui.EndChild()
-		// End Right
+		imgui.EndGroup()
+		// FileName End
 	}
-	imgui.EndChild()
+	imgui.EndGroup()
 }
