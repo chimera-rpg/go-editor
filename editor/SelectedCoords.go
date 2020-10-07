c104b54a0c324bbbc67c5e378538965a8541e8c9
kts of kettek (yan) <kettek1@kettek.net>
2020-10-07 14:57:54 -0700

    Add SelectedCoords type

---
+++ editor/SelectedCoords.go
@@ -0,0 +1,26 @@
+package editor
+
+// SelectedCoords is a simple structure that provides functionality to select/unselect arbitrary coordinates.
+type SelectedCoords struct {
+	selected map[[3]int]struct{}
+}
+
+// Get returns the underlying map.
+func (s *SelectedCoords) Get() map[[3]int]struct{} {
+	return s.selected
+}
+
+// Select selects the given coordinates.
+func (s *SelectedCoords) Select(y, x, z int) {
+	s.selected[[3]int{y, x, z}] = struct{}{}
+}
+
+// Unselect unselects the given coordinates.
+func (s *SelectedCoords) Unselect(y, x, z int) {
+	delete(s.selected, [3]int{y, x, z})
+}
+
+// Clear clears all selected coordinates.
+func (s *SelectedCoords) Clear() {
+	s.selected = make(map[[3]int]struct{})
+}
